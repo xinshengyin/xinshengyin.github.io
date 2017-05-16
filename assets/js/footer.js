3d7a42669458fa35d322a15d9776a0a569e4eae8
qinmingyuan <mingyuan0715@foxmail.com>
2017-05-16 15:55:16 +0800

    add turbolinks js

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,9 +1,8 @@
-$(document).ready(function(){
+document.addEventListener('turbolinks:load', function(){
 
   function side1() {
     $('.ui.side').addClass('invisible');
-    $('.ui.side .ui.menu').removeClass('accordion');
-    $('.ui.side .ui.menu').addClass('icon');
+    $('.ui.side .ui.menu').removeClass('accordion').addClass('icon');
     $('.ui.side .ui.menu .dropdown.icon').remove();
     $('.ui.side .ui.menu div.item:not(.header)').addClass('ui dropdown');
     $('.ui.dropdown.item').dropdown({on: 'hover'});
@@ -12,21 +11,29 @@ $(document).ready(function(){
 
   function side2() {
     $('.ui.side').removeClass('invisible');
-    $('.ui.side .ui.menu').addClass('accordion');
-    $('.ui.side .ui.menu').removeClass('icon');
-    $('.ui.side .ui.menu .item:not(.header)').removeClass('ui dropdown');
-    $('.ui.side .ui.menu .title').append('<i class="dropdown icon"></i>');
+    $('.ui.side .ui.menu').addClass('accordion').removeClass('icon');
+    $('.ui.side .ui.menu .item:not(.header)').removeClass('ui dropdown').dropdown('destroy');
+    var title = $('.ui.side .ui.menu .title');
+    title.children('i.dropdown.icon').remove();
+    title.append('<i class="dropdown icon"></i>');
     $('.ui.accordion').accordion({selector: {trigger: '.title'}});
-    $('.ui.side .ui.menu .item:not(.header)').dropdown('destroy');
   }
 
   $('#close_side').click(function() {
     if ($('.ui.side').hasClass('invisible')) {
+      window.localStorage.setItem('invisible', 'false');
       side2();
     } else {
+      window.localStorage.setItem('invisible', 'true');
       side1();
     }
   });
+
+  if (localStorage.getItem('invisible') === 'true') {
+    side1();
+  } else {
+    side2();
+  }
 
 });
 
